cbcb2be8a92bace6688c386695d040a1868327a3
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-08 16:55:59 +0300

    basic routing using pages router

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,20 @@
-import React from 'react';
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
