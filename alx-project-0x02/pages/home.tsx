1b0bb43961c077848f5095e7a5044135ca70433f
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-08 17:20:46 +0300

    update the card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,5 +1,14 @@
-const HomePage = () =>{
-    return <h1> Home Page </h1>
-}
+import Card from "@/components/common/Card";
+
+const HomePage = () => {
+  return(
+  <div>
+    <h1> Home Page </h1>
+    <Card title="Card one" content="First card"/>
+    <Card title="Card two" content="Second card"/>
+    <Card title="Card three" content="Third card"/>
+  </div>
+  );
+};
 
 export default HomePage;
